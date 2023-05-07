0e271815c2c987505ef86d3658abcd1811b9c803
the_barista <keclapperton@gmail.com>
2023-05-07 19:20:11 -0400

    Making other tests with try catch

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,60 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// Function to handle errors
+	let handleError = (error: unknown) => { 
+		if (error instanceof Error){ 
+			vscode.window.showInformationMessage(`Test error: ${error.name} -> ${error.message}`);
+		}
+		else { 
+			vscode.window.showInformationMessage(`Test error: Unknown error`); 
+		}
+	};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-api-functional-test" is now active!');
+	// Tests 
+	let commandCreateController = vscode.commands.registerCommand('test-api-functional-test.runAllTest', () => {
+		let ctrl: vscode.TestController; 
+		const rqst = new vscode.TestRunRequest();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-api-functional-test.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Test API functional test!');
+		try {
+			ctrl = vscode.tests.createTestController('controllerId', 'myTestController');
+			vscode.window.showInformationMessage(`SUCCESS TestController created: ${ctrl.label}`);
+		} catch (error) {
+			vscode.window.showInformationMessage(`FAIL Could not create TestController`);
+			handleError(error);
+			throw new Error("Tests failed")
+		}
+
+		try { 
+			const token = new vscode.CancellationTokenSource().token;
+			const kind = vscode.TestRunProfileKind.Run;
+			const profile = ctrl.createRunProfile("myRunProfile", kind, (rqst, token)=>void{})
+			vscode.window.showInformationMessage(`SUCCESS created TestRunProfile: ${ctrl.label}`);
+		}catch (error) { 
+			vscode.window.showInformationMessage(`FAIL Could not create TestRunProfile`)
+			handleError(error);
+			throw new Error("Tests failed")
+		}
+
+		try { 
+			const testItem = ctrl.createTestItem("testItemId", "testItemLabel");
+			vscode.window.showInformationMessage(`SUCCESS created TestItem: ${testItem.id}`);
+		}catch (error) { 
+			vscode.window.showInformationMessage(`FAIL Could not create TestItem`)
+			handleError(error);
+			throw new Error("Tests failed")
+		}
+
+		try { 
+			const testRun = ctrl.createTestRun(rqst, "testRunName");
+			vscode.window.showInformationMessage(`SUCCESS created TestRun: ${testRun.name}`);
+		}catch (error) { 
+			vscode.window.showInformationMessage(`FAIL Could not create TestRun`)
+			handleError(error);
+			throw new Error("Tests failed")
+		}
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(commandCreateController);
 }
 
 // This method is called when your extension is deactivated
